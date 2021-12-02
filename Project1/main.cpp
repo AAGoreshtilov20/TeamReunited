62bcc109d8e39ab6d7d263f6020336e67bfe2e62
PRPenchev20 <PRPenchev20@codingburgas.bg>
2021-12-02 22:29:46 +0200

    Small changes

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,12 +3,11 @@
 #include <iomanip>
 #include <cmath>
 #include <cstring>
-#include <stdlib.h>
 #include <time.h>
 using namespace std;
 int main()
 {
-    int md, su, sp, prnm, stt, st, rm, kpe, nl, hnl;
+    //int md, su, sp, prnm, stt, st, rm, kpe, nl, hnl;
     int N, i;
     cout << "Pick a topic:" << endl;
     cout << "1 - Mechanical movement" << endl;
@@ -22,7 +21,7 @@ int main()
     cout << "9 - Pressure and Paskal's law" << endl;
     cout << "10 - Hydrostatic pressure" << endl;
     
-
+    cout << "Enter number: ";
     cin >> N;
     switch (N)
     {
