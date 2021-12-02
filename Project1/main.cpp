44974e7a3d5ed61874334b2f3c8cccaa3551a186
MRShivachev20 <mrshivachev20@codingburgas.bg>
2021-12-02 22:13:35 +0200

    Add cases

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,10 +2,14 @@
 #include <cstring>
 #include <iomanip>
 #include <cmath>
+#include <cstring>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 int main()
 {
-    int md, su, sp, prnm, stt, st, rm, kpe, nl, hnl, N;
+    int md, su, sp, prnm, stt, st, rm, kpe, nl, hnl;
+    int N, i;
     cout << "Pick a topic:" << endl;
     cout << "1 - Mechanical movement" << endl;
     cout << "2 - Speed and acceleration" << endl;
@@ -22,37 +26,237 @@ int main()
     cin >> N;
     switch (N)
     {
-    case 1 : 
-        cout << "1";
+    case 1:
+        srand((unsigned)time(NULL));
+       
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1: 
+            cout << "A1";
+            break;
+        case 2:
+            cout << "A2";
+            break;
+        case 3:
+            cout << "A3";
+            break;
+        case 4:
+            cout << "A4";
+            break;
+        case 5:
+            cout << "A5";
+            break;
+       }    
         break;
-    case 2 :
-        cout << "2";
+    case 2:
+        srand((unsigned)time(NULL));
+        
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "B1";
+            break;
+        case 2:
+            cout << "B2";
+            break;
+        case 3:
+            cout << "B3";
+            break;
+        case 4:
+            cout << "B4";
+            break;
+        case 5:
+            cout << "B5";
+            break;
+        }
         break;
-    case 3 :
-        cout << "3";
+    case 3:
+        srand((unsigned)time(NULL));
+        
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "C1";
+            break;
+        case 2:
+            cout << "C2";
+            break;
+        case 3:
+            cout << "C3";
+            break;
+        case 4:
+            cout << "C4";
+            break;
+        case 5:
+            cout << "C5";
+            break;
+        }
         break;
-    case 4 :
-        cout << "4";
+    case 4:
+        srand((unsigned)time(NULL));
+        
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "D1";
+            break;
+        case 2:
+            cout << "D2";
+            break;
+        case 3:
+            cout << "D3";
+            break;
+        case 4:
+            cout << "D4";
+            break;
+        case 5:
+            cout << "D5";
+            break;
+        }
         break;
-    case 5 :
-        cout << "5";
+    case 5:
+        srand((unsigned)time(NULL));
+        
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "E1";
+            break;
+        case 2:
+            cout << "E2";
+            break;
+        case 3:
+            cout << "E3";
+            break;
+        case 4:
+            cout << "E4";
+            break;
+        case 5:
+            cout << "E5";
+            break;
+        }
         break;
-    case 6 :
-        cout << "6";
+    case 6:
+        srand((unsigned)time(NULL));
+       
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "F1";
+            break;
+        case 2:
+            cout << "F2";
+            break;
+        case 3:
+            cout << "F3";
+            break;
+        case 4:
+            cout << "F4";
+            break;
+        case 5:
+            cout << "F5";
+            break;
+        }
         break;
-    case 7 :
-        cout << "7";
+    case 7:
+        srand((unsigned)time(NULL));
+        
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "G1";
+            break;
+        case 2:
+            cout << "G2";
+            break;
+        case 3:
+            cout << "G3";
+            break;
+        case 4:
+            cout << "G4";
+            break;
+        case 5:
+            cout << "G5";
+            break;
+        }
         break;
-    case 8 :
-        cout << "8";
+    case 8:
+        srand((unsigned)time(NULL));
+       
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "H1";
+            break;
+        case 2:
+            cout << "H2";
+            break;
+        case 3:
+            cout << "H3";
+            break;
+        case 4:
+            cout << "H4";
+            break;
+        case 5:
+            cout << "H5";
+            break;
+        }
         break;
-    case 9 :
-        cout << "9";
+    case 9:
+        srand((unsigned)time(NULL));
+        
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "I1";
+            break;
+        case 2:
+            cout << "I2";
+            break;
+        case 3:
+            cout << "I3";
+            break;
+        case 4:
+            cout << "I4";
+            break;
+        case 5:
+            cout << "I5";
+            break;
+        }
         break;
-    case 10 :
-        cout << "10";
+    case 10:
+        srand((unsigned)time(NULL));
+        
+        i = (rand() % 5) + 1;
+        switch (i)
+        {
+        case 1:
+            cout << "J1";
+            break;
+        case 2:
+            cout << "J2";
+            break;
+        case 3:
+            cout << "J3";
+            break;
+        case 4:
+            cout << "J4";
+            break;
+        case 5:
+            cout << "J5";
+            break;
+        }
         break;
     default:
-        cout << "yes";
+        cout << "Wrong input";
     }
 }
